2b662c1046c5c3286c387d628bf60dc01c13d286
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-26 18:56:40 +0530

    ADDED : Welcome message to the program in master branch

---
+++ src/main/java/com/censusAnalyserProgram/services/CensusAnalyserMain.java
@@ -0,0 +1,7 @@
+package com.censusAnalyserProgram.services;
+
+public class CensusAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Indian States Census Analyser Problem");
+    }
+}
