a3d969648c50ca876f6a94c059b98eb7b8249e48
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-27 00:33:57 +0530

    Update StateCensusAnalyser.java

--- src/main/java/com/censusAnalyserProgram/services/StateCensusAnalyser.java
+++ src/main/java/com/censusAnalyserProgram/services/StateCensusAnalyser.java
@@ -3,7 +3,6 @@ package com.censusAnalyserProgram.services;
 import com.opencsv.bean.CsvBindByName;
 
 public class StateCensusAnalyser {
-    public class IndiaCensusGettingPOJO {
         @CsvBindByName(column = "State name")
         private String stateName;
         @CsvBindByName(column = "Population")
@@ -54,5 +53,4 @@ public class StateCensusAnalyser {
                     ", femaleLiterate=" + femaleLiterate +
                     '}';
         }
-    }
 }
