ffbb651e71e0278c8a861aeef0952637f15aecd8
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-27 00:30:03 +0530

    ADDED : created sad test case for custom exception

--- src/test/java/com/censusAnalyserProgram/services/IndianCensusAnalyserTest.java
+++ src/test/java/com/censusAnalyserProgram/services/IndianCensusAnalyserTest.java
@@ -2,12 +2,12 @@ package com.censusAnalyserProgram.services;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.rules.ExpectedException;
 
 public class IndianCensusAnalyserTest {
 
     private static final String ORIGINAL_FILE_PATH = "C:\\Users\\ACER\\Desktop\\New folder\\Census_Analyser_Problem\\src\\main\\resources\\india-districts-census-2011.csv";
+    private static final String WRONG_FILE_PATH = "C:\\Users\\ACER\\Desktop\\New folder\\Census_Analyser_Problem\\src\\main\\test\\india-districts-census-2011.csv";
 
     @Test
     public void givenCorrectOrNote_Checked(){
@@ -18,6 +18,17 @@ public class IndianCensusAnalyserTest {
         } catch (CensusAnalyserException e){}
     }
 
-
+    @Test
+    public void givenWrongPathReturnsCustomException(){
+        try{
+            IndianCensusAnalyser censusAnalyser = new IndianCensusAnalyser();
+            ExpectedException expectedRule =  ExpectedException.none();
+            expectedRule.expect(CensusAnalyserException.class);
+            int numberOfRecords = censusAnalyser.loadIndiaCensusData(WRONG_FILE_PATH);
+            Assert.assertEquals(640, numberOfRecords);
+        }catch (CensusAnalyserException e){
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
 
 }
